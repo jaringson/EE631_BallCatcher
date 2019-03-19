3f7911657b6bb94c00719dc54d81e61d70cb6356
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-18 21:27:50 -0600

    Started doing some of the stuff for moments

--- tracker.cpp
+++ tracker.cpp
@@ -1,1 +1,24 @@
 #include "tracker.h"
+
+Tracker::Tracker(cv::Mat imgL, cv::Mat imgR)
+{
+  imgL.copyTo(_backgroundL);
+  imgR.copyTo(_backgroundR);
+}
+
+void Tracker::setImages(cv::Mat imgL, cv::Mat imgR)
+{
+  imgL.copyTo(_imgL);
+  imgR.copyTo(_imgR);
+}
+
+void Tracker::calcFinalPosition()
+{
+  cv::Point2f centerL{calcMoment(_imgL)};
+  cv::Point2f centerR{calcMoment(_imgR)};
+}
+
+cv::Point2f Tracker::calcMoment(const cv::Mat & img)
+{
+
+}
--- tracker.h
+++ tracker.h
@@ -4,15 +4,22 @@
 #include <cmath>
 #include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 class Tracker
 {
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  Tracker();
-  ~Tracker();
+
+  Tracker(cv::Mat imgL, cv::Mat imgR);
+  void setImages(cv::Mat imgL, cv::Mat imgR);
+  void calcFinalPosition();
 
 private:
+  cv::Point2f calcMoment(const cv::Mat& img);
+
+  cv::Mat _backgroundL, _backgroundR;
+  cv::Mat _imgL, _imgR;
 
 };
 
