d0adb564866854c9ea4028c14d9f07664f7d33ae
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-18 21:05:34 -0600

    Started the libarary

---
+++ tracker.h
@@ -0,0 +1,19 @@
+#ifndef TRACKER
+#define TRACKER
+
+#include <cmath>
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+
+class Tracker
+{
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  Tracker();
+  ~Tracker();
+
+private:
+
+};
+
+#endif
