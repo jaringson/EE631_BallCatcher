4315d9c22e1b8cad631ed4713233df2430e08e52
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-18 21:41:26 -0600

    Calculated the moment

--- tracker.cpp
+++ tracker.cpp
@@ -29,6 +29,11 @@ cv::Point2f Tracker::calcMoment(cv::Mat img, cv::Mat background)
   g_img = absoluteDifference(g_img, background);
   g_img = computeThreshold(g_img, 20);
   g_img = cleanUpNoise(g_img);
+
+  cv::Moments m{cv::moments(g_img, true)};
+  cv::Point2f center{m.m10/m.m00, m.m01/m.m00};
+
+  return center;
 }
 
 cv::Mat Tracker::absoluteDifference(cv::Mat gray_frame, cv::Mat prev_frame)
