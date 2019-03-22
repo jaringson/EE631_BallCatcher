5584d0d62e6f2036d0d5482533ae109832b6a9d7
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-22 11:30:30 -0600

    initialized the tracker object

--- loadVideo.cpp
+++ loadVideo.cpp
@@ -1,8 +1,19 @@
 #include "tracker.h"
 #include <opencv2/opencv.hpp>
+#include <string>
+
+#define DEBUG
 
 int main()
 {
+  std::string filenameL("../baseball/ballL"), filenameR("../baseball/ballR");
+  std::string file_ext(".bmp");
+
+  cv::Mat imgL, imgR, backgroundL, backgroundR;
+  imgL = cv::imread(filenameL + "00" + file_ext);
+  imgR = cv::imread(filenameR + "00" + file_ext);
+
+  Tracker tracker(imgL, imgR);
 
   return 0;
 }
